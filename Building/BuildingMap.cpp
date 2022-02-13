ddea7577dfe633e8802bbfc7167f6bfb9021dcd5
Gustavo Augusto <gacl@ic.ufal.br>
2022-02-13 14:10:00 -0300

    Adicionado textura nos prédios

--- Building/BuildingMap.cpp
+++ Building/BuildingMap.cpp
@@ -1,9 +1,11 @@
 #include "BuildingMap.h"
 
 #include <GL/glu.h>
+#include "../texture.h"
 
-Building::Building(int i, int j, int h) {
+Building::Building(int i, int j, int h, Texture texture_1) {
   x = i;
   y = j;
   height = h;
+  texture = texture_1;
 }
--- Building/BuildingMap.h
+++ Building/BuildingMap.h
@@ -1,12 +1,15 @@
 #ifndef PLANE_PROJECT_BUILDING_H
 #define PLANE_PROJECT_BUILDING_H
 
+#include "../texture.h"
+
 class Building {
   public:
     int x;
     int y;
     int height;
-    Building(int i, int j, int h);
+    Texture texture;
+    Building(int i, int j, int h, Texture texture_1);
 };
 
 #endif
